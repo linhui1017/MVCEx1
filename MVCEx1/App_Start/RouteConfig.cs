adb741a297f8c58701c73e432f7c78b80bdd1784
linhui1017 <s9043044@gmail.com>
2015-11-11 12:06:16 +0800

    客戶資料清單完成

--- MVCEx1/App_Start/RouteConfig.cs
+++ MVCEx1/App_Start/RouteConfig.cs
@@ -7,17 +7,23 @@ using System.Web.Routing;
 
 namespace MVCEx1
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+			routes.MapRoute(
+				name: "Coutomer",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "客戶資料View", action = "Index", id = UrlParameter.Optional }
+			);
+
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
 }
